7cef85ea457ce3fff6f7df88be061c7875132dad
Noah Golub <ngolub@stanford.edu>
2019-10-07 13:52:22 -0700

    fixed warnings in standalone ledger

--- src/bin/ledger_standalone/main.rs
+++ src/bin/ledger_standalone/main.rs
@@ -1,10 +1,12 @@
 use api_service::RestfulApiService;
 use ledger::store::LedgerState;
 use std::sync::{Arc, RwLock};
-use std::thread;
 
 fn main() {
-  let mut ledger = LedgerState::test_ledger();
+  let ledger = LedgerState::test_ledger();
   let query_service = RestfulApiService::create(Arc::new(RwLock::new(ledger))).unwrap();
-  query_service.run();
+  match query_service.run() {
+    Ok(_) => {}
+    Err(_) => println!("Error running query service"),
+  }
 }
