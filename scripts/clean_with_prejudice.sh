6a48908b5835e0f7126733f98831e02aba2ade6f
Joe Doyle <joe@findora.org>
2020-01-21 14:59:25 -0800

    Fix macos find compatibility, suppress double-deletion messages

--- scripts/clean_with_prejudice.sh
+++ scripts/clean_with_prejudice.sh
@@ -9,9 +9,11 @@ GIT_ROOT="$(git rev-parse --show-toplevel)"
 pushd $GIT_ROOT >/dev/null
 
 # Delete all files named 'Cargo.lock' in the tree below
-find -type f -name 'Cargo.lock' -print0 | xargs -0 rm -v
+find . -type f -name 'Cargo.lock' -print0 | xargs -r -n 1 -0 rm -v
 # Delete all directories named 'target' in the tree below
-find -type d -name 'target' -print0 | xargs -0 rm -rv
+# Sorting in reverse order suppresses error messages by deleting
+#   .../target/.../target before .../target
+find . -type d -name 'target' -print0 | sort -z -r | xargs -r -n 1 -0 rm -rv
 
 # return to original working directory
 popd >/dev/null
