e38caab1176f4bde292ec4c14652fa564a41f4e3
Keyao Shen <shenkeyao@gmail.com>
2020-02-01 20:29:21 -0800

    Add copyright notice.

--- components/codes/src/codes.rs
+++ components/codes/src/codes.rs
@@ -1,77 +1,66 @@
-// Exit codes and descriptions are copied from:
-// https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+// Reference: /usr/include/sysexits.h
+
+/*
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)sysexits.h	8.1 (Berkeley) 6/2/93
+ */
+
 pub mod exit_code {
-  // The command was used incorrectly, e.g., with the
-  // wrong number of arguments, a bad flag, a bad syntax
-  // in a parameter, or whatever.
+  pub const EX_OK: i32 = 0;
+
   pub const EX_USAGE: i32 = 64;
 
-  // The input data was incorrect in some way.  This
-  // should only be used for user's data and not system
-  // files.
   pub const EX_DATAERR: i32 = 65;
 
-  // An input file (not a system file) did not exist or
-  // was not readable.  This could also include errors
-  // like ``No message'' to a mailer (if it cared to
-  // catch it).
   pub const EX_NOINPUT: i32 = 66;
 
-  // The user specified did not exist.  This might be
-  // used for mail addresses or remote logins.
   pub const EX_NOUSER: i32 = 67;
 
-  // The host specified did not exist.  This is used in
-  // mail addresses or network requests.
   pub const EX_NOHOST: i32 = 68;
 
-  // A service is unavailable.  This can occur if a sup­
-  // port program or file does not exist.  This can also
-  // be used as a catchall message when something you
-  // wanted to do doesn't work, but you don't know why.
   pub const EX_UNAVAILABLE: i32 = 69;
 
-  // An internal software error has been detected.  This
-  // should be limited to non-operating system related
-  // errors as possible.
   pub const EX_SOFTWARE: i32 = 70;
 
-  // An operating system error has been detected.  This
-  // is intended to be used for such things as ``cannot
-  // fork'', ``cannot create pipe'', or the like.  It
-  // includes things like getuid returning a user that
-  // does not exist in the passwd file.
   pub const EX_OSERR: i32 = 71;
 
-  // Some system file (e.g., /etc/passwd, /var/run/utmp,
-  // etc.) does not exist, cannot be opened, or has some
-  // sort of error (e.g., syntax error).
   pub const EX_OSFILE: i32 = 72;
 
-  // A (user specified) output file cannot be created.
   pub const EX_CANTCREATE: i32 = 73;
 
-  // An error occurred while doing I/O on some file.
   pub const EX_IOERR: i32 = 74;
 
-  // Temporary failure, indicating something that is not
-  // really an error.  In sendmail, this means that a
-  // mailer (e.g.) could not create a connection, and
-  // the request should be reattempted later.
   pub const EX_TEMPFAIL: i32 = 75;
 
-  // The remote system returned something that was ``not
-  // possible'' during a protocol exchange.
   pub const EX_PROTOCOL: i32 = 76;
 
-  // You did not have sufficient permission to perform
-  // the operation.  This is not intended for file sys­
-  // tem problems, which should use EX_NOINPUT or
-  // EX_CANTCREAT, but rather for higher level permis­
-  // sions.
   pub const EX_NOPERM: i32 = 77;
 
-  // Something was found in an unconfigured or miscon­
-  // figured state.
   pub const EX_CONFIG: i32 = 78;
 }
