2dd53e83288b13788110cc48489ec30029d9cafc
Keyao Shen <shenkeyao@gmail.com>
2020-05-21 03:18:52 -0700

    Fix 2 TODOs, and refer the issue # for the remaining TODO

--- components/txn_cli/src/txn_lib.rs
+++ components/txn_cli/src/txn_lib.rs
@@ -43,11 +43,10 @@ pub mod txn_lib {
   extern crate exitcode;
 
   /// Initial data when the program starts.
-  // TODO (Keyao):
+  // TODO (Keyao): Redmine issue: #42: Store txn_cli data externally
   // Make this data driven, not embedded in the Rust code.
   // The attribute names will be determined by the customer's application and will differ from customer to customer.
   // Or we'll develop a standard registry or dictionary of attributes.
-  // Either way, this will be stored externally.
   const INIT_DATA: &str = r#"
 {
   "asset_issuers": [
@@ -160,7 +159,7 @@ pub mod txn_lib {
     /// lower bound of the income
     MinIncome = 1,
     /// Country code of citizenship
-    // TODO (Keyao): Add a reference for the country code definition.
+    /// See https://countrycode.org/ for country code definition.
     Citizenship = 2,
   }
 
@@ -393,7 +392,6 @@ pub mod txn_lib {
 
   #[derive(Clone, Deserialize, Debug, Serialize)]
   /// Loan information.
-  // TODO (Keyao): Remove unused fields
   pub(crate) struct Loan {
     /// Loan ID
     id: u64,
@@ -421,14 +419,6 @@ pub mod txn_lib {
     code: Option<String>,
     /// Debt asset UTXO (unspent transaction output) SIDs, null if the loan isn't fulfilled     
     debt_utxo: Option<TxoSID>,
-    /// Serialized anon_creds::Credential, null if the loan isn't fulfilled     
-    credential: Option<String>,
-    /// Serialized credential user secret key, if exists
-    user_secret_key: Option<String>,
-    /// Serialized credential signature, if exists
-    signature: Option<String>,
-    /// Serialized credential commitment key, if exists
-    commitment_key: Option<String>,
   }
 
   impl Loan {
@@ -450,11 +440,7 @@ pub mod txn_lib {
              duration,
              payments: 0,
              code: None,
-             debt_utxo: None,
-             credential: None,
-             user_secret_key: None,
-             signature: None,
-             commitment_key: None }
+             debt_utxo: None }
     }
   }
 
@@ -1770,12 +1756,10 @@ pub mod txn_lib {
     let mut prng: ChaChaRng = ChaChaRng::from_entropy();
     let (user_pk, user_secret_key) =
       credential_user_key_gen(&mut prng, &credential_issuer_public_key);
-    let user_sk_str = serde_json::to_vec(&user_secret_key).or_else(|_| Err(ser_fail!()))?;
     let signature = credential_sign(&mut prng,
                                     &credential_issuer_secret_key,
                                     &user_pk,
                                     &attributes).unwrap();
-    let signature_str = serde_json::to_string(&signature).or_else(|_| Err(ser_fail!()))?;
     let wrapper_credential = WrapperCredential { attributes: attibutes_with_value_as_vec,
                                                  issuer_pub_key:
                                                    credential_issuer_public_key.clone(),
@@ -1785,13 +1769,6 @@ pub mod txn_lib {
                         .or_else(|e| Err(PlatformError::ZeiError(error_location!(), e)))?;
     let (identity_commitment, _, commitment_key) =
       credential_commit(&mut prng, &user_secret_key, &wrapper_credential, b"").unwrap();
-    let commitment_key_str = serde_json::to_vec(&commitment_key).or_else(|_| Err(ser_fail!()))?;
-
-    // Update credential data
-    data.loans[loan_id as usize].user_secret_key = Some(hex::encode(user_sk_str));
-    data.loans[loan_id as usize].signature = Some(signature_str);
-    data.loans[loan_id as usize].commitment_key = Some(hex::encode(commitment_key_str));
-    store_data_to_file(data.clone(), data_dir)?;
 
     // Store the tracer memo to file
     if let Some(file) = memo_file {
@@ -1969,14 +1946,12 @@ pub mod txn_lib {
              sids_new[0].0, fiat_sid_merged.0);
 
     // Update data
-    let credential_str = serde_json::to_string(&ac_credential).or_else(|_| Err(ser_fail!()))?;
     let mut data = load_data(data_dir)?;
     data.loans[loan_id as usize].issuer = Some(issuer_id);
     data.fiat_code = Some(fiat_code.to_base64());
     data.loans[loan_id as usize].status = LoanStatus::Active;
     data.loans[loan_id as usize].code = Some(debt_code.to_base64());
     data.loans[loan_id as usize].debt_utxo = Some(sids_new[0]);
-    data.loans[loan_id as usize].credential = Some(credential_str);
     data.borrowers[borrower_id as usize].balance = borrower.balance + amount;
     data.borrowers[borrower_id as usize].fiat_utxo = Some(fiat_sid_merged);
     store_data_to_file(data, data_dir)
