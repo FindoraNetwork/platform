5d1f1b495c368bfd4fcb2876ec7da55caa29fe3f
Nathan Yospe <nathan@eian.io>
2020-05-29 13:31:08 -0700

    behaving more like the non-tendermint ledger

--- components/abci_validator_node/src/abci_validator_node.rs
+++ components/abci_validator_node/src/abci_validator_node.rs
@@ -5,12 +5,14 @@ use ledger::data_model::Transaction;
 use ledger::store::*;
 use ledger_api_service::RestfulApiService;
 use log::info;
+use std::path::Path;
 use rand_chacha::ChaChaRng;
 use rand_core::SeedableRng;
 use std::net::SocketAddr;
 use std::process::Command;
 use std::sync::{Arc, RwLock};
 use std::thread;
+use submission_api::SubmissionApi;
 use submission_server::{convert_tx, SubmissionServer, TxnForward};
 use utils::HashOf;
 
@@ -32,17 +34,20 @@ impl TxnForward for TendermintForward {
 }
 
 struct ABCISubmissionServer {
-  la: SubmissionServer<ChaChaRng, LedgerState, TendermintForward>,
+  la: Arc<RwLock<SubmissionServer<ChaChaRng, LedgerState, TendermintForward>>>,
 }
 
 impl ABCISubmissionServer {
-  fn new() -> Result<ABCISubmissionServer, PlatformError> {
-    let ledger = LedgerState::test_ledger();
+  fn new(base_dir: Option<&Path>) -> Result<ABCISubmissionServer, PlatformError> {
+  let ledger_state = match base_dir {
+    None => LedgerState::test_ledger(),
+    Some(base_dir) => LedgerState::load_or_init(base_dir).unwrap(),
+  };
     let prng = rand_chacha::ChaChaRng::from_entropy();
     Ok(ABCISubmissionServer { la:
-                                SubmissionServer::new_no_auto_commit(prng,
-                                                                     Arc::new(RwLock::new(ledger)),
-                                                                     Some(TendermintForward::default()))? })
+                                Arc::new(RwLock::new(SubmissionServer::new_no_auto_commit(prng,
+                                                                     Arc::new(RwLock::new(ledger_state)),
+                                                                     Some(TendermintForward::default()))?)) })
   }
 }
 
@@ -53,11 +58,13 @@ impl abci::Application for ABCISubmissionServer {
     let mut resp = ResponseCheckTx::new();
 
     if let Some(tx) = convert_tx(req.get_tx()) {
-      if self.la.get_committed_state().write().is_ok() {
+      if let Ok(la) = self.la.read() {
+        if la.get_committed_state().write().is_ok() {
         if TxnEffect::compute_effect(tx).is_err() {
           resp.set_code(1);
           resp.set_log(String::from("Check failed"));
         }
+      }
       } else {
         resp.set_code(1);
         resp.set_log(String::from("Could not access ledger"));
@@ -74,37 +81,40 @@ impl abci::Application for ABCISubmissionServer {
     // Get the Tx [u8]
     let mut resp = ResponseDeliverTx::new();
     if let Some(tx) = convert_tx(req.get_tx()) {
-      if self.la.cache_transaction(tx).is_ok() {
+      if let Ok(mut la) = self.la.write() { if la.cache_transaction(tx).is_ok() {
         return resp;
       }
+    }
     }
     resp.set_code(1);
     resp
   }
 
   fn begin_block(&mut self, _req: &RequestBeginBlock) -> ResponseBeginBlock {
-    self.la.begin_block();
+    if let Ok(mut la) = self.la.write() { la.begin_block(); }
     ResponseBeginBlock::new()
   }
 
   fn end_block(&mut self, _req: &RequestEndBlock) -> ResponseEndBlock {
     // TODO: this should propagate errors instead of panicking
-    self.la.end_block().unwrap();
+    if let Ok(mut la) = self.la.write() { la.end_block().unwrap(); }
     ResponseEndBlock::new()
   }
 
   fn commit(&mut self, _req: &RequestCommit) -> ResponseCommit {
     // Tendermint does not accept an error return type here.
     let error_commitment = (HashOf::new(&None), 0);
-    self.la.begin_commit();
-    let commitment = if let Ok(state) = self.la.get_committed_state().read() {
+    let mut r = ResponseCommit::new();
+    if let Ok(mut la) = self.la.write() {
+    la.begin_commit();
+    let commitment = if let Ok(state) = la.get_committed_state().read() {
       state.get_state_commitment()
     } else {
       error_commitment
     };
-    self.la.end_commit();
-    let mut r = ResponseCommit::new();
+    la.end_commit();
     r.data = commitment.0.as_ref().to_vec();
+  }
     r
   }
 
@@ -121,16 +131,41 @@ fn main() {
   // let addr = "127.0.0.1:26658".parse().unwrap();
 
   // abci::run(addr, ABCISubmissionServer::default());
-  let app = ABCISubmissionServer::new().unwrap();
-  let ledger_state = app.la.borrowable_ledger_state();
-  let host = std::option_env!("SERVER_HOST").unwrap_or("localhost");
-  let port = std::option_env!("SERVER_PORT").unwrap_or("8668");
+  flexi_logger::Logger::with_env().start().unwrap();
+  let base_dir = std::env::var_os("LEDGER_DIR").filter(|x| !x.is_empty());
+  let base_dir = base_dir.as_ref().map(Path::new);
+  let app = ABCISubmissionServer::new(base_dir).unwrap();
+  let submission_server = Arc::clone(&app.la);
+  let cloned_lock = Arc::clone(&submission_server.read().unwrap().borrowable_ledger_state());
 
-  let _join = thread::spawn(move || {
-    let query_service = RestfulApiService::create(ledger_state, host, port)?;
-    query_service.run()
+  let host = std::env::var_os("SERVER_HOST").filter(|x| !x.is_empty())
+                                            .unwrap_or_else(|| "localhost".into());
+  let host2 = host.clone();
+  let submission_port = std::env::var_os("SUBMISSION_PORT").filter(|x| !x.is_empty())
+                                                           .unwrap_or_else(|| "8669".into());
+  let ledger_port = std::env::var_os("LEDGER_PORT").filter(|x| !x.is_empty())
+                                                   .unwrap_or_else(|| "8668".into());
+  thread::spawn(move || {
+    let submission_api = SubmissionApi::create(submission_server,
+                                               host.to_str().unwrap(),
+                                               submission_port.to_str().unwrap()).unwrap();
+    println!("Starting submission service");
+    match submission_api.run() {
+      Ok(_) => println!("Successfully ran submission service"),
+      Err(_) => println!("Error running submission service"),
+    }
   });
 
+  let _join = thread::spawn(move || {
+    let query_service = RestfulApiService::create(cloned_lock,
+                                                 host2.to_str().unwrap(),
+                                                 ledger_port.to_str().unwrap()).unwrap();
+  println!("Starting ledger service");
+  match query_service.run() {
+    Ok(_) => println!("Successfully ran standalone"),
+    Err(_) => println!("Error running standalone"),
+  }
+  });
 
   let abci_host = std::option_env!("ABCI_HOST").unwrap_or("0.0.0.0");
   let abci_port = std::option_env!("ABCI_PORT").unwrap_or("26658");
