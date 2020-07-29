143c5c44fb032a9f61e9b90e7fd6647da9805326
Philippe Camacho <philippe@findora.org>
2020-07-29 16:04:27 -0400

    Remove echo commands.

--- components/cli2/tests/cli.sh
+++ components/cli2/tests/cli.sh
@@ -6,14 +6,12 @@ rm -f $FINDORA_STORE_FILE
 
 @test "key generation" {
   run $CLI2 key-gen alice
-  echo ${lines[0]}
   [ "$status" -eq 0 ]
   [ "${lines[0]}" = 'New key pair added for `alice`' ]
 }
 
 @test "add bob's public key" {
   run bash -c 'echo "\"i4-1NC50E4omcPdO4N28v7cBvp0pnPOFp6Jvyu4G3J4=\"" | $CLI2 load-public-key bob'
-  echo "${lines[0]}"
   [ "$status" -eq 0 ]
   [ "${lines[0]}" = 'New public key added for `bob`' ]
 }
