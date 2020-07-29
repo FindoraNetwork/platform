440368d59f1fa8e8d87e04d4e3e78d56d5128e3e
Philippe Camacho <philippe@findora.org>
2020-07-29 15:59:04 -0400

    BB test case for adding a public key.

--- components/cli2/tests/cli.sh
+++ components/cli2/tests/cli.sh
@@ -11,3 +11,9 @@ rm $FINDORA_STORE_FILE
   [ "${lines[0]}" = 'New key pair added for `alice`' ]
 }
 
+@test "add bob's public key" {
+  run bash -c 'echo "\"i4-1NC50E4omcPdO4N28v7cBvp0pnPOFp6Jvyu4G3J4=\"" | $CLI2 load-public-key bob'
+  echo "${lines[0]}"
+  [ "$status" -eq 0 ]
+  [ "${lines[0]}" = 'New public key added for `bob`' ]
+}
