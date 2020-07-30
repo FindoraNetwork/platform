926e8c187adf896eabbe5f602b3953909e0d7861
Philippe Camacho <philippe@findora.org>
2020-07-29 22:30:14 -0400

    List public keys BB test.

--- components/cli2/tests/cli.sh
+++ components/cli2/tests/cli.sh
@@ -21,6 +21,15 @@ setup() {
   [ "${lines[0]}" = 'New public key added for `bob`' ]
 }
 
+@test "list public keys" {
+  run bash -c 'echo "\"i4-1NC50E4omcPdO4N28v7cBvp0pnPOFp6Jvyu4G3J4=\"" | $CLI2 load-public-key bob'
+  run bash -c 'echo "\"CaOPNpTSFitNXoyxpsfL-amF_lHanegLIAUTkNsA2yw==\"" | $CLI2 load-public-key greg'
+  run $CLI2 list-public-key bob
+  [ "$status" -eq 0 ]
+#  $CLI2 list-public-key greg
+#  [ "$status" -eq 0 ]
+}
+
 @test "list the key pairs" {
   run bash -c '$CLI2 key-gen bob; $CLI2 list-keypair bob'
   [ "$status" -eq 0 ]
@@ -40,4 +49,14 @@ setup() {
   $CLI2 list-txn-builders;
   [ "$status" -eq 0 ]
   [ "${lines[9]}" = 'Done.' ] #TODO better capture of output
+  run bash -c
+}
+
+
+@test "issue asset" {
+  skip "Not implemented"
+  run bash -c '$CLI2 key-gen alice; echo y | $CLI2 query-ledger-state; $CLI2 prepare-transaction 0;echo memo0 | $CLI2 define-asset alice AliceCoin --txn 0;'
+  [ "$status" -eq 0 ]
+  $CLI2 issue-asset --txn=0 alice AliceCoin 1000
+  [ "$status" -eq 0 ]
 }
