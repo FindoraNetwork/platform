42fd79866b10801a2aafe18cdd7b4a258db4f95d
jpdoyle <joe@findora.org>
2020-07-29 17:56:12 -0700

    Add nix-run-tests.sh

---
+++ components/cli2/nix-run-tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+exec nix-shell -p bats bc --command 'BATS=`which bats` CLI2=`pwd`/../../target/debug/cli bash ./run_tests.sh'
--- components/cli2/run_tests.sh
+++ components/cli2/run_tests.sh
@@ -1,3 +1,5 @@
+set -euo pipefail
+
 export FINDORA_HOME=$(mktemp -d)
 echo "FINDORA_HOME: ${FINDORA_HOME}"
 
@@ -10,4 +12,9 @@ cargo test
 #  black box tests written in shell
 $BATS tests/hello_world.sh
 $BATS tests/cli.sh
+
+rm -d $FINDORA_HOME || {
+  rm $FINDORA_HOME/cli2_data.sqlite;
+  rm -d $FINDORA_HOME;
+}
 
