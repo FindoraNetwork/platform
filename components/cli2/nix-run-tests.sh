5650fa8a4844116672d63de43f4e2f29d6a761a0
jpdoyle <joe@findora.org>
2020-08-03 08:54:39 -0700

    `cli` -> `findora`

--- components/cli2/nix-run-tests.sh
+++ components/cli2/nix-run-tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec nix-shell -p bats bc --command 'BATS=`which bats` CLI2=`pwd`/../../target/debug/cli bash ./run_tests.sh'
+exec nix-shell -p bats bc --command 'BATS=`which bats` CLI2=`pwd`/../../target/debug/findora bash ./run_tests.sh'
