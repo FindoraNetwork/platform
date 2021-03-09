2653a2fdcedfc5d510adf6f029a930a5d9f51a2e
teefin <tavakyan@protonmail.com>
2021-03-08 21:50:59 -0800

    fmt

--- components/wasm/src/wasm.rs
+++ components/wasm/src/wasm.rs
@@ -1506,7 +1506,7 @@ mod test {
     #[test]
     fn test_asset_rules_to_str() {
         let mut ar = AssetRules {
-            rules: Default::default()
+            rules: Default::default(),
         };
         ar.rules.max_units = Some(10000000000 as u64);
         let actual_serialized_json = serde_json::to_string(&ar.rules.clone()).unwrap();
@@ -1514,12 +1514,11 @@ mod test {
         assert_eq!(actual_serialized_json, expected_serialized_json);
     }
 
-
     #[test]
     fn test_asset_rules_from_str() {
         use ledger::data_model::AssetRules as PlatformAssetRules;
         let mut ar = AssetRules {
-            rules: Default::default()
+            rules: Default::default(),
         };
         let amt = 10000000000 as u64;
         ar.rules.max_units = Some(amt);
@@ -1527,7 +1526,10 @@ mod test {
         let expected_serialized_json = r#"{"transferable":true,"updatable":false,"transfer_multisig_rules":null,"max_units":"10000000000","decimals":6}"#.to_string();
         assert_eq!(actual_serialized_json, expected_serialized_json);
 
-        let res: PlatformAssetRules = serde_json::from_str::<PlatformAssetRules>(&expected_serialized_json.to_string()).unwrap();
+        let res: PlatformAssetRules = serde_json::from_str::<PlatformAssetRules>(
+            &expected_serialized_json.to_string(),
+        )
+        .unwrap();
         assert_eq!(res.max_units.unwrap(), amt);
     }
 
@@ -1535,13 +1537,15 @@ mod test {
     fn test_asset_rules_from_str_null_max_units() {
         use ledger::data_model::AssetRules as PlatformAssetRules;
         let mut ar = AssetRules {
-            rules: Default::default()
+            rules: Default::default(),
         };
         let amt = 10000000000 as u64;
         ar.rules.max_units = Some(amt);
         let actual_serialized_json = r#"{"transferable":true,"updatable":false,"transfer_multisig_rules":null,"max_units":null,"decimals":6}"#.to_string();
 
-        let res: PlatformAssetRules = serde_json::from_str::<PlatformAssetRules>(&actual_serialized_json.clone()).unwrap();
+        let res: PlatformAssetRules =
+            serde_json::from_str::<PlatformAssetRules>(&actual_serialized_json.clone())
+                .unwrap();
         assert_eq!(res.max_units, None);
     }
 }
