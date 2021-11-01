a7cde05ba805a70fe0fc47a841d118a45e7c1f73
FanHui <hui@findora.org>
2021-11-01 19:44:46 +0800

    remove useless components

--- src/components/frog/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use {
-    ledger::data_model::{TxoSID, Utxo},
-    std::collections::BTreeMap,
-    zei::xfr::{sig::XfrPublicKey, structs::OwnerMemo},
-};
-
-pub type UtxoMap = BTreeMap<XfrPublicKey, BTreeMap<TxoSID, (Utxo, Option<OwnerMemo>)>>;
