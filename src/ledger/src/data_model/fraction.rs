cbb2b612f3f92bc8a66540f9383a5c21ef6dd2fc
shaitao <sht892197>
2021-12-08 21:49:19 +0800

    implement overflowing arithmetic, remove saturating arithmatic.

--- src/ledger/src/data_model/fraction.rs
+++ src/ledger/src/data_model/fraction.rs
@@ -2,7 +2,7 @@
 
 use core::cmp::Ordering;
 use core::fmt;
-use core::ops::{Add, Mul, Sub};
+use core::ops::{Add, Mul};
 
 use ruc::*;
 
@@ -34,6 +34,38 @@ fn gcd(mut x: Repr, mut y: Repr) -> Repr {
     x
 }
 
+fn overflowing_add(a: Repr, b: Repr) -> Result<Repr> {
+    let (r, o) = Repr::overflowing_add(a, b);
+    if o {
+        return Err(eg!("Addition overflowing!."));
+    }
+    Ok(r)
+}
+
+fn overflowing_sub(a: Repr, b: Repr) -> Result<Repr> {
+    let (r, o) = Repr::overflowing_sub(a, b);
+    if o {
+        return Err(eg!("Substration overflowing!."));
+    }
+    Ok(r)
+}
+
+fn overflowing_mul(a: Repr, b: Repr) -> Result<Repr> {
+    let (r, o) = Repr::overflowing_mul(a, b);
+    if o {
+        return Err(eg!("Multiplication overflowing!."));
+    }
+    Ok(r)
+}
+
+//overflowing never occur.
+fn checked_div(a: Repr, b: Repr) -> Result<(Repr, Repr)> {
+    if b == 0 {
+        return Err(eg!("Dividing by zero!"));
+    }
+    Ok((a / b, a % b))
+}
+
 impl U128Fraction {
     ///Give out a zero.
     pub fn zero() -> Self {
@@ -45,16 +77,11 @@ impl U128Fraction {
         if d == 0 {
             return Err(eg!("Denominator is zero!"));
         }
-
-        Ok(U128Fraction::new_unchecked(n, d))
-    }
-
-    fn new_unchecked(n: Repr, d: Repr) -> Self {
         if n == 0 {
-            return U128Fraction::zero();
+            return Ok(U128Fraction::zero());
         }
         let g = gcd(n, d);
-        U128Fraction(n / g, d / g)
+        Ok(U128Fraction(n / g, d / g))
     }
 
     ///Estimate the value of the fracton.
@@ -62,9 +89,9 @@ impl U128Fraction {
         self.0 as f64 / self.1 as f64
     }
 
-    ///Return the quotient and remainder of this fraction.
-    pub fn quotient(&self) -> (Repr, Repr) {
-        (self.0 / self.1, self.0 % self.1)
+    ///Return the quotient and remainder of this fraction, Error will be returned if denominator is zero.
+    pub fn quotient(&self) -> Result<(Repr, Repr)> {
+        checked_div(self.0, self.1)
     }
 
     ///Consume itself and return a `[Repr;2]`.
@@ -73,34 +100,41 @@ impl U128Fraction {
     }
 
     ///Calculate: a/b <op> c/d = (ad <op> bc)/(bd)
-    fn saturating_op(self, rhs: Self, op: impl Fn(Repr, Repr) -> Repr) -> Self {
+    fn overflowing_op(
+        self,
+        rhs: Self,
+        op: impl Fn(Repr, Repr) -> Result<Repr>,
+    ) -> Result<Self> {
         if self.1 == rhs.1 {
-            return U128Fraction::new_unchecked(op(self.0, rhs.0), self.1);
+            return U128Fraction::new(op(self.0, rhs.0)?, self.1);
         }
 
-        let num = op(self.0.saturating_mul(rhs.1), self.1.saturating_mul(rhs.0));
-        let div = Repr::saturating_mul(self.1, rhs.1);
-        U128Fraction::new_unchecked(num, div)
+        let num = op(
+            overflowing_mul(self.0, rhs.1)?,
+            overflowing_mul(self.1, rhs.0)?,
+        )?;
+        let div = overflowing_mul(self.1, rhs.1)?;
+        U128Fraction::new(num, div)
     }
 
-    ///Saturating addition with an U64Frac. Computes `self + rhs`.
-    pub fn saturating_add(self, rhs: Self) -> Self {
-        self.saturating_op(rhs, Repr::saturating_add)
+    ///Overflowing addition with an U64Frac. Computes `self + rhs`, Error will be returned if overflowing.
+    pub fn overflowing_add(self, rhs: Self) -> Result<Self> {
+        self.overflowing_op(rhs, overflowing_add)
     }
 
-    ///Saturating substraction with an U64Frac. Computes `self - rhs`.
-    pub fn saturating_sub(self, rhs: Self) -> Self {
-        self.saturating_op(rhs, Repr::saturating_sub)
+    ///Overflowing substraction with an U64Frac. Computes `self - rhs`, Error will be returned if overflowing.
+    pub fn overflowing_sub(self, rhs: Self) -> Result<Self> {
+        self.overflowing_op(rhs, overflowing_sub)
     }
 
-    ///Saturating multiplication with an U64Frac. Computes `self * rhs`.
-    pub fn saturating_mul(self, rhs: Self) -> Self {
+    ///Overflowing multiplication with an U64Frac. Computes `self * rhs`, Error will be returned if overflowing.
+    pub fn overflowing_mul(self, rhs: Self) -> Result<Self> {
         if self.0 == 0 || rhs.0 == 0 {
-            return U128Fraction::zero();
+            return Ok(U128Fraction::zero());
         }
-        let num = self.0.saturating_mul(rhs.0);
-        let div = self.1.saturating_mul(rhs.1);
-        U128Fraction::new_unchecked(num, div)
+        let num = overflowing_mul(self.0, rhs.0)?;
+        let div = overflowing_mul(self.1, rhs.1)?;
+        U128Fraction::new(num, div)
     }
 }
 
@@ -117,51 +151,34 @@ impl Eq for U128Fraction {}
 
 impl PartialOrd for U128Fraction {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(
-            Repr::saturating_mul(self.0, other.1)
-                .cmp(&Repr::saturating_mul(self.1, other.0)),
-        )
-    }
-}
-
-impl Ord for U128Fraction {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
+        let (r1, o1) = Repr::overflowing_mul(self.0, other.1);
+        let (r2, o2) = Repr::overflowing_mul(self.1, other.0);
+        if o1 || o2 {
+            return None;
+        }
+        Some(r1.cmp(&r2))
     }
 }
 
 impl Add<Repr> for U128Fraction {
-    type Output = Self;
+    type Output = Result<Self>;
 
-    fn add(self, other: Repr) -> Self {
-        U128Fraction::new_unchecked(
-            Repr::saturating_add(self.0, other.saturating_mul(self.1)),
-            self.1,
-        )
-    }
-}
-
-impl Sub<Repr> for U128Fraction {
-    type Output = Self;
-
-    fn sub(self, other: Repr) -> Self {
-        U128Fraction::new_unchecked(
-            Repr::saturating_sub(self.0, other.saturating_mul(self.1)),
-            self.1,
-        )
+    fn add(self, other: Repr) -> Self::Output {
+        let n = overflowing_mul(self.1, other)?;
+        U128Fraction::new(overflowing_add(self.0, n)?, self.1)
     }
 }
 
 impl Mul<Repr> for U128Fraction {
-    type Output = Self;
+    type Output = Result<Self>;
 
-    fn mul(self, other: Repr) -> Self {
-        U128Fraction::new_unchecked(self.0.saturating_mul(other), self.1)
+    fn mul(self, other: Repr) -> Self::Output {
+        U128Fraction::new(overflowing_mul(self.0, other)?, self.1)
     }
 }
 
 impl Mul<U128Fraction> for u128 {
-    type Output = U128Fraction;
+    type Output = Result<U128Fraction>;
     fn mul(self, other: U128Fraction) -> Self::Output {
         other * self
     }
@@ -185,7 +202,7 @@ impl TryFrom<(Repr, Repr)> for U128Fraction {
 
 impl From<Repr> for U128Fraction {
     fn from(rhs: Repr) -> Self {
-        U128Fraction::new_unchecked(rhs, 1)
+        U128Fraction::new(rhs, 1).unwrap()
     }
 }
 
@@ -196,27 +213,51 @@ mod test {
     fn test_calculation() {
         //get 1/2
         let half = U128Fraction::new(125, 250).unwrap();
-        assert_eq!(half.saturating_sub(half), U128Fraction::zero());
+        assert_eq!(half.overflowing_sub(half).unwrap(), U128Fraction::zero());
         // 1/4 = 1/2 * 1/2
-        let quarter = half.saturating_mul(half);
+        let quarter = half.overflowing_mul(half).unwrap();
         println!("{}", quarter);
         // 1/4 = 1/2 - 1/4
-        assert_eq!(quarter, half.saturating_sub(quarter));
+        assert_eq!(quarter, half.overflowing_sub(quarter).unwrap());
         assert!((quarter.estimate_f64() - 0.25_f64).abs() < f64::EPSILON);
         // 1/4 < 1/2
         assert!(quarter < half);
 
-        let x = quarter * 7_u128;
+        let x = (quarter * 7_u128).unwrap();
         println!("{}", x);
         assert!(x > half);
+        assert!(half < x);
 
         // 7/4 = 1/4 + 3/2
-        assert_eq!(x, quarter.saturating_add(U128Fraction::new(3, 2).unwrap()));
+        assert_eq!(
+            x,
+            quarter
+                .overflowing_add(U128Fraction::new(3, 2).unwrap())
+                .unwrap()
+        );
         // 7/4 + 1 = 11/4
-        assert_eq!(x + 1, U128Fraction::new(11, 4).unwrap());
+        assert_eq!((x + 1).unwrap(), U128Fraction::new(11, 4).unwrap());
         // 7/4 = 1, remainder = 3
-        let (q, r) = x.quotient();
+        let (q, r) = x.quotient().unwrap();
         assert_eq!(q, 1);
         assert_eq!(r, 3);
+
+        let max_v = U128Fraction::new(u128::MAX, 1).unwrap();
+
+        assert!(max_v.overflowing_mul(max_v).is_err());
+        assert!(max_v.overflowing_add(max_v).is_err());
+        assert!((max_v * 2).is_err());
+        assert!((max_v + 10).is_err());
+
+        //Comparing always return false because of overflowing.
+        assert!(
+            !(U128Fraction::new(u128::MAX, 27).unwrap()
+                < U128Fraction::new(u128::MAX, 5).unwrap())
+        );
+
+        assert!(
+            !(U128Fraction::new(u128::MAX, 27).unwrap()
+                 > U128Fraction::new(u128::MAX, 5).unwrap())
+        );
     }
 }
