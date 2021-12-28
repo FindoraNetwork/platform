ec915f35168d7dacb83f3eb66ddf2950ad398b35
charlie <charlie@findora.org>
2021-12-27 22:43:38 -0500

    adjusted height to 1495900

--- src/components/abciapp/src/abci/server/callback/mod.rs
+++ src/components/abciapp/src/abci/server/callback/mod.rs
@@ -65,7 +65,7 @@ pub const DISBALE_EVM_BLOCK_HEIGHT: i64 = 148_3286;
 pub const ENABLE_FRC20_HEIGHT: i64 = 1;
 
 #[cfg(not(feature = "debug_env"))]
-pub const ENABLE_FRC20_HEIGHT: i64 = 149_5960;
+pub const ENABLE_FRC20_HEIGHT: i64 = 1495900;
 
 pub fn info(s: &mut ABCISubmissionServer, req: &RequestInfo) -> ResponseInfo {
     let mut resp = ResponseInfo::new();
