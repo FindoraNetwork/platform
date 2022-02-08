6383643adf5b43b25335a13be37c06f2bfa33201
kevinssgh <kevin.singh@findora.org>
2022-02-08 11:56:25 -0500

    Fixed transfer command

--- tools/regression/evm.py
+++ tools/regression/evm.py
@@ -28,7 +28,7 @@ def get_balance(arguments):
         balance = get_erc20_balance(addr)
     elif sec_key is not None:
         balance = get_fra_balance(sec_key)
-    # print('{}balance of {} is {}{}'.format(OKBLUE, account, balance, ENDC))
+    print('{}balance is {} wei {}'.format(OKBLUE, balance, ENDC))
     return balance
 
 
@@ -42,28 +42,26 @@ def verify_balance(arguments):
         print("{}Balance Verified successfully{}".format(OKGREEN, ENDC))
 
 
-# def transfer(arguments):
-#     w3 = Web3(HTTPProvider(to_web3_url(local)))
-#     signed_txn = w3.eth.account.sign_transaction(dict(
-#             nonce=w3.eth.get_transaction_count(arguments['from_addr']),
-#             maxFeePerGas=3000000000,
-#             maxPriorityFeePerGas=2000000000,
-#             gas=100000,
-#             to=arguments['to_addr'],
-#             value=int(arguments['amount']),
-#             data=b'',
-#             type=2,  # (optional) the type is now implicitly set based on appropriate transaction params
-#             chainId=1,
-#         ),
-#         arguments['from_priv_key'],
-#     )
-#     w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-#     return
+def transfer(arguments):
+    w3 = Web3(HTTPProvider(to_web3_url(local)))
+    signed_txn = w3.eth.account.sign_transaction(dict(
+            nonce=w3.eth.get_transaction_count(arguments['from_addr']),
+            gas=100000,
+            gasPrice=10000000000,
+            to=arguments['to_addr'],
+            value=int(arguments['amount']),
+            data=b'',
+            chainId=523,
+        ),
+        arguments['from_priv_key'],
+    )
+    w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    return
 
 
 if __name__ == "__main__":
     # initialize commands
-    cmd_list = {'balance': get_balance, 'verify-balance': verify_balance}  # 'transfer': transfer
+    cmd_list = {'balance': get_balance, 'verify-balance': verify_balance, 'transfer': transfer}  # 'transfer': transfer
 
     # Initialize Parser
     parser = argparse.ArgumentParser()
@@ -80,12 +78,12 @@ if __name__ == "__main__":
     parser_verify_balance.add_argument('--sec-key', help='secret key of FRA account')
     parser_verify_balance.add_argument('--amount', default='0', help='expected balance for given address')
 
-    # # Initialize Transfer Parser
-    # parser_transfer = subparsers.add_parser('transfer', help='transfer funds between two erc20 addresses')
-    # parser_transfer.add_argument('--from_addr', help='address to transfer funds from')
-    # parser_transfer.add_argument('--from_priv_key', help='address to transfer funds from')
-    # parser_transfer.add_argument('--to_addr', help='address to transfer funds to')
-    # parser_transfer.add_argument('--amount', help='amount of funds to transfer')
+    # Initialize Transfer Parser
+    parser_transfer = subparsers.add_parser('transfer', help='transfer funds between two erc20 addresses')
+    parser_transfer.add_argument('--from_addr', help='address to transfer funds from')
+    parser_transfer.add_argument('--from_priv_key', help='address to transfer funds from')
+    parser_transfer.add_argument('--to_addr', help='address to transfer funds to')
+    parser_transfer.add_argument('--amount', help='amount of funds to transfer')
 
     # Execute Command
     args = vars(parser.parse_args())
