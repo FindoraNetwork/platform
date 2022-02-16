98d5a3114a66600332d96277214e58b22cc3f0dd
Harshad Patil <harshad0402@gmail.com>
2022-02-15 16:00:30 -0800

    Fix nullifier hash serialization

--- src/components/wasm/src/wasm.rs
+++ src/components/wasm/src/wasm.rs
@@ -937,14 +937,8 @@ pub fn gen_nullifier_hash(
         &oabar.get_asset_type(),
         mt_leaf_info.get_zei_mt_leaf_info().uid,
     );
-    let input = base64::encode_config(&n.to_bytes(), base64::URL_SAFE);
-    let nullifier_hash = sha256::Digest::from_slice(
-        &base64::decode_config(&input, base64::URL_SAFE)
-            .c(d!())
-            .unwrap(),
-    );
-
-    Ok(serde_json::to_string(&nullifier_hash).unwrap())
+    let hash = base64::encode_config(&n.to_bytes(), base64::URL_SAFE);
+    Ok(hash)
 }
 
 #[wasm_bindgen]
