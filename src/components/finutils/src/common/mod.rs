cab0467e36ac736c247ea00056317e327bd4c997
Harshad Patil <harshad0402@gmail.com>
2022-04-07 15:54:43 -0700

    Fix asset mixing abar demo (#277)
    * Fix Abar asset mixing demo script
    
    1. Added more sleep for txns to go through
    2. fix parsing of newly created assets
    3. reading BARs through CLI after removing hard-coding
    
    * fmt
    
    * fix clippy

--- src/components/finutils/src/common/mod.rs
+++ src/components/finutils/src/common/mod.rs
@@ -20,7 +20,7 @@ use {
     ledger::{
         data_model::{
             gen_random_keypair, ATxoSID, AssetRules, AssetTypeCode, Transaction, TxoSID,
-            BLACK_HOLE_PUBKEY_STAKING,
+            ASSET_TYPE_FRA, BLACK_HOLE_PUBKEY_STAKING,
         },
         staking::{
             check_delegation_amount, td_addr_to_bytes, td_pubkey_to_td_addr,
@@ -1151,7 +1151,9 @@ pub fn gen_oabar_add_op_x(
 
         // Get OwnerMemo
         let axtxo_abar = utils::get_owned_abars(&from_pub_key_randomized).c(d!())?;
-        let owner_memo = utils::get_abar_memo(&axtxo_abar[0].0).c(d!())?.unwrap();
+        let owner_memo = utils::get_abar_memo(&axtxo_abar[0].0)
+            .c(d!(randomizers[i]))?
+            .unwrap();
         // Get Merkle Proof
         let mt_leaf_info = utils::get_abar_proof(&axtxo_abar[0].0).c(d!())?.unwrap();
         let mt_leaf_uid = mt_leaf_info.uid;
@@ -1287,16 +1289,41 @@ pub fn get_abar_memo(uid: &ATxoSID) -> Result<Option<OwnerMemo>> {
 }
 
 /// Fetches list of owned TxoSIDs from LedgerStatus
-pub fn get_owned_utxos() -> Result<Vec<(TxoSID, XfrAmount, XfrAssetType)>> {
+pub fn get_owned_utxos(
+    asset: Option<&str>,
+) -> Result<Vec<(TxoSID, XfrAmount, XfrAssetType)>> {
+    // get KeyPair from current setup wallet
     let kp = get_keypair().c(d!())?;
 
-    let list = utils::get_owned_utxos(&kp.pub_key)?
-        .iter()
-        .map(|a| {
-            let record = a.1.clone().0 .0.record;
-            (*a.0, record.amount, record.asset_type)
-        })
-        .collect();
+    // Parse Asset Type for filtering if provided
+    let mut asset_type = ASSET_TYPE_FRA;
+    if let Some(a) = asset {
+        asset_type = if a.to_uppercase() == "FRA" {
+            ASSET_TYPE_FRA
+        } else {
+            AssetTypeCode::new_from_base64(asset.unwrap()).unwrap().val
+        };
+    }
+
+    let list: Vec<(TxoSID, XfrAmount, XfrAssetType)> =
+        utils::get_owned_utxos(&kp.pub_key)?
+            .iter()
+            .filter(|a| {
+                // Filter by asset type if given or read all
+                if asset.is_none() {
+                    true
+                } else {
+                    match a.1.clone().0 .0.record.asset_type {
+                        XfrAssetType::Confidential(_) => false,
+                        XfrAssetType::NonConfidential(x) => asset_type == x,
+                    }
+                }
+            })
+            .map(|a| {
+                let record = a.1.clone().0 .0.record;
+                (*a.0, record.amount, record.asset_type)
+            })
+            .collect();
 
     Ok(list)
 }
@@ -1356,7 +1383,7 @@ pub fn anon_balance(
     println!();
     println!(
         "{0: <8} | {1: <18} | {2: <45} | {3: <9} | {4: <45} | {5: <45}",
-        "ATxoSID","Amount", "AssetType", "IsSpent", "AXfrPublicKey", "Randomizer"
+        "ATxoSID", "Amount", "AssetType", "IsSpent", "AXfrPublicKey", "Randomizer"
     );
     println!("{:-^1$}", "", 184);
     randomizers_list
@@ -1392,7 +1419,7 @@ pub fn anon_balance(
                     AssetTypeCode {
                         val: oabar.get_asset_type()
                     }
-                        .to_base64(),
+                    .to_base64(),
                     null_status,
                     axfr_public_key_str,
                     r
