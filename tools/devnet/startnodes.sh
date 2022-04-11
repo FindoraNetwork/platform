cb96c38707e983ca2a8bab36d0c6ca26d792c8aa
simonjiao <simon@findora.org>
2022-04-12 01:03:45 +0800

    add profiler

--- tools/devnet/startnodes.sh
+++ tools/devnet/startnodes.sh
@@ -3,46 +3,47 @@
 # env
 source ./tools/devnet/env.sh || exit 1
 
+if [ -f "checkpoint.toml" ]; then
+  mv checkpoint.toml checkpoint.toml.bak
+fi
+
 # start one single node if specified
 Node=""
 if [ ! -z "$1" ]; then
-    Node="node$1"
+  Node="node$1"
 fi
 
 # start abcis
-nodes=`ls -l $DEVNET | grep node  | awk '(NR>0){print $9}' | sort -V`
-for node in $nodes
-do
-if [ -z "$Node" ] || ([ ! -z "$Node" ] && [ "$Node" = "$node" ]); then
-        SelfAddr=$(grep 'address' ${DEVNET}/${node}/config/priv_validator_key.json | grep -oE '[^",]{40}')
-        TD_NODE_SELF_ADDR=$SelfAddr \
-        RUST_LOG=$ABCI_LOG_LEVEL \
-        LEDGER_DIR=$DEVNET/$node/abci \
-        ENABLE_LEDGER_SERVICE=true \
-        ENABLE_ETH_API_SERVICE=true \
-        abcid $DEVNET/$node >> $DEVNET/$node/abcid.log 2>&1  &
-fi
+nodes=$(ls -l $DEVNET | grep node | awk '(NR>0){print $9}' | sort -V)
+for node in $nodes; do
+  if [ -z "$Node" ] || ([ ! -z "$Node" ] && [ "$Node" = "$node" ]); then
+    SelfAddr=$(grep 'address' ${DEVNET}/${node}/config/priv_validator_key.json | grep -oE '[^",]{40}')
+    TD_NODE_SELF_ADDR=$SelfAddr \
+      RUST_LOG=$ABCI_LOG_LEVEL \
+      LEDGER_DIR=$DEVNET/$node/abci \
+      ENABLE_LEDGER_SERVICE=true \
+      ENABLE_ETH_API_SERVICE=true \
+      abcid $DEVNET/$node >>$DEVNET/$node/abcid.log 2>&1 &
+  fi
 done
 
 # start nodes
-for node in $nodes
-do
-    perl -pi -e 's/addr_book_strict\s*=.*/addr_book_strict = false/g' $DEVNET/$node/config/config.toml || exit 1
-    if [[ "" != ${DEBUG_ENV_IP} ]]; then
-        perl -pi -e "s/127.0.0.1/${DEBUG_ENV_IP}/g" $DEVNET/$node/config/config.toml || exit 1
-    fi
-    tendermint node --home $DEVNET/$node >> $DEVNET/$node/consensus.log 2>&1  &
+for node in $nodes; do
+  perl -pi -e 's/addr_book_strict\s*=.*/addr_book_strict = false/g' $DEVNET/$node/config/config.toml || exit 1
+  if [[ "" != ${DEBUG_ENV_IP} ]]; then
+    perl -pi -e "s/127.0.0.1/${DEBUG_ENV_IP}/g" $DEVNET/$node/config/config.toml || exit 1
+  fi
+  tendermint node --home $DEVNET/$node >>$DEVNET/$node/consensus.log 2>&1 &
 done
 
 # show abcis and nodes
-for node in $nodes
-do
-if [ -z "$Node" ] || ([ ! -z "$Node" ] && [ "$Node" = "$node" ]); then
+for node in $nodes; do
+  if [ -z "$Node" ] || ([ ! -z "$Node" ] && [ "$Node" = "$node" ]); then
     echo -n "$node: "
-    abci=`pgrep -f "abcid $DEVNET/$node$" | tr "\n" " " | xargs echo -n`
+    abci=$(pgrep -f "abcid $DEVNET/$node$" | tr "\n" " " | xargs echo -n)
     echo -en "abci(${GRN}$abci${NC}) <---> "
     sleep 0.2
-    node=`pgrep -f "tendermint node --home $DEVNET/$node$" | tr "\n" " " | xargs echo -n`
+    node=$(pgrep -f "tendermint node --home $DEVNET/$node$" | tr "\n" " " | xargs echo -n)
     echo -e "node(${GRN}$node${NC})"
-fi
+  fi
 done
