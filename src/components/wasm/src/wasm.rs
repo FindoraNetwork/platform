60a88ad67fc11a382d91c3ec5b66b87d0d83ec68
Sun <sun@findora.org>
2022-04-14 11:41:30 +0800

    update wasm from_string

--- src/components/wasm/src/wasm.rs
+++ src/components/wasm/src/wasm.rs
@@ -329,11 +329,10 @@ impl TransactionBuilder {
     /// Deserialize transaction builder from string.
     pub fn from_string(s: String) -> Result<TransactionBuilder, JsValue> {
         let transaction_builder = serde_json::from_str(&s).map_err(error_to_jsvalue)?;
-        let randomizers = Default::default();
 
         Ok(TransactionBuilder {
             transaction_builder,
-            randomizers,
+            commitments: Default::default(),
         })
     }
 
