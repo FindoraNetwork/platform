3941628e720647e132b55345255c147dd1cbb8ab
tiannian <dtiannian@gmail.com>
2022-04-17 11:13:15 +0800

    Fix error of prism (#291)

--- src/components/contracts/modules/evm/src/runtime/runner.rs
+++ src/components/contracts/modules/evm/src/runtime/runner.rs
@@ -237,7 +237,12 @@ impl<C: Config> ActionRunner<C> {
         if let ExitReason::Succeed(_) = result {
             Ok((data, state.substate.logs, gas_used))
         } else {
-            Err(eg!("Execute system error: {:?}", result))
+            // TODO: store error execution on pending tx later.
+            Err(eg!(
+                "Execute system error: {:?}, data is: {}",
+                result,
+                hex::encode(data)
+            ))
         }
     }
 }
