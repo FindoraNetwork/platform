03233fc65c926880febf914790e383b881419aa1
simonjiao <simon@findora.org>
2022-04-25 16:57:47 +0800

    remove some database operations

--- src/components/abciapp/src/abci/server/callback/mod.rs
+++ src/components/abciapp/src/abci/server/callback/mod.rs
@@ -424,7 +424,7 @@ pub fn commit(s: &mut ABCISubmissionServer, req: &RequestCommit) -> ResponseComm
     {
         let mut guard_locked = PROFILER_GUARD.lock();
         let guard = guard_locked.get_mut();
-        if let Some(_) = guard
+        if guard
             .as_ref()
             .and_then(|(_, guard)| guard.report().build().ok())
             .and_then(|report| {
@@ -436,6 +436,7 @@ pub fn commit(s: &mut ABCISubmissionServer, req: &RequestCommit) -> ResponseComm
                 .ok()
             })
             .and_then(|(report, file)| report.flamegraph(file).ok())
+            .is_some()
         {
             log::info!(target: "abciapp", "write flamegraph.h{}.svg", td_height);
         } else {
