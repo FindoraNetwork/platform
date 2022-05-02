36dbd03dfd1e4e590092be64c13aba20ddb97457
hridambasu <hridam.basu@gmail.com>
2022-05-03 00:38:11 +0530

    balances working
    Feature to verify assets for custom balances
    
    adding asset filter to `fn account`
    
    multi-asset basic test working
    
    remove virtual env files
    
    some fixes, refactoring and cleanup
    
    adding support for anon balance check
    
    CLI changes for regression test
    Integrated anon balance CLI in regression test

--- tools/regression/triple_masking/scripts/setup_wallets.sh
+++ tools/regression/triple_masking/scripts/setup_wallets.sh
@@ -10,7 +10,7 @@ echo "
   \"axfr_public_key\": \"zQa8j0mGYUXM6JxjWN_pqfOi1lvEyenJYq35OIJNN08=\",
   \"enc_key\": \"Gu558brzFchoqQR9oi8QP54KZKSQ18Djzt82C4YUyFg=\",
   \"dec_key\": \"4GNC0J_qOXV2kww5BC5bOCyrTEfCodX5BoFaj06uN1s=\"
-}" > anon-keys-temp.keys
+}" > "$FILE_ANON_KEYS"
 
 fn setup -O mnemonic-temp.keys -S http://0.0.0.0
 
@@ -25,5 +25,14 @@ echo "
   \"axfr_public_key\": \"ptyo7hlqn-Eywf_ttG99OSWtz6KfDUvLYDDVDlV1C-U=\",
   \"enc_key\": \"SAmB7Oji4sAgENLaLb4PFclxQL_DRrEkXcYp6eXuXwI=\",
   \"dec_key\": \"AEq1ZUFk_fB__YaNjQ3D2taGOnMZAx4adpB6RbnPj24=\"
-}" >> anon-keys-temp2.keys
-sleep 5
+}" > "$FILE_ANON_KEYS_2"
+
+echo "
+ {
+  \"axfr_secret_key\": \"vw41h9OciN5cDh8QSPFEodSA5AnuHxxcV5SKJ_q-JQMGUX_qogo-lXUvhnXo1gYuP8YsoJJro7kNloXx8wfDSA==\",
+  \"axfr_public_key\": \"BlF_6qIKPpV1L4Z16NYGLj_GLKCSa6O5DZaF8fMHw0g=\",
+  \"enc_key\": \"NBY5yIhdriJVq-7BS59J2IxBgLhewr8TEE6suNc1elA=\",
+  \"dec_key\": \"OKh4F5o_Mw0eKi0xU8lQhi_lXMVd-hem7N12lvSy_WU=\"
+}
+"  > "$FILE_ANON_KEYS_3"
+sleep 1
