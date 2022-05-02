c752e328f635528e354f017c81e5e4ec43c50212
hridambasu <hridam.basu@gmail.com>
2022-05-03 00:38:11 +0530

    balances working

--- tools/regression/triple_masking/test_triple_masking.sh
+++ tools/regression/triple_masking/test_triple_masking.sh
@@ -33,13 +33,13 @@ fn convert-bar-to-abar --anon-keys ./$FILE_ANON_KEYS  --txo-sid $TXO_SID
 sleep 20
 
 #Verify
-python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 839990000
+python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 839980000
 echo
 
 commitment1=$(tail -n 1 owned_commitments)
 echo "\n\n Owned Abars after Bar to Abar conversion"
-sleep 20 #Do not remove/decrease
-fn owned-abars --commitment $commitment1
+sleep 40 #Do not remove/decrease
+fn owned-abars -c $commitment1
 
 
 echo "\n\n\n Anonymous Transfer from Sender1 to Receiver1"
@@ -48,9 +48,9 @@ fn anon-transfer --amount 189990000 --anon-keys ./anon-keys-temp.keys --to-axfr-
 
 commitment2=$(tail -n 1 sent_commitments)
 echo "\n\n Owned Abars for Receiver1 after Anon Transfer 1"
-sleep 30
+sleep 40
 echo $commitment2 > /dev/null
-fn owned-abars --commitment $commitment2
+fn owned-abars -c $commitment2
 
 echo "\n\n\n Anonymous Transfer from Receiver1 (Sender2) to Receiver2"
 echo "------------------------------------------------------------------------------"
@@ -58,9 +58,9 @@ fn anon-transfer --amount 169990000 --anon-keys ./anon-keys-temp2.keys --to-axfr
 
 commitment3=$(tail -n 1 sent_commitments)
 echo "\n\n Owned Abars for Receiver2 after Anon Transfer 2"
-sleep 30
+sleep 40
 echo $commitment3 > /dev/null
-fn owned-abars --commitment $commitment3
+fn owned-abars -c $commitment3
 
 sleep 2
 echo "\n\n\n Fetch merkle proof for Anon Transfer 2"
@@ -68,7 +68,7 @@ echo "--------------------------------------------------------------------------
 fn anon-fetch-merkle-proof -a 2
 
 #Verify
-python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 839990000
+python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 839980000
 echo
 
 fn owned-utxos
@@ -82,26 +82,26 @@ TXO_SID=$(target/release/fn owned-utxos | head -4 | tail -1 |  awk -F ' ' '{prin
 fn convert-bar-to-abar --anon-keys ./$FILE_ANON_KEYS  --txo-sid $TXO_SID
 sleep 5
 
-TXO_SID=$(target/release/fn owned-utxos | head -4 | tail -1 |  awk -F ' ' '{print $1}')
-fn convert-bar-to-abar --anon-keys ./$FILE_ANON_KEYS  --txo-sid $TXO_SID
-sleep 5
+#TXO_SID=$(target/release/fn owned-utxos | head -4 | tail -1 |  awk -F ' ' '{print $1}')
+#fn convert-bar-to-abar --anon-keys ./$FILE_ANON_KEYS  --txo-sid $TXO_SID
+#sleep 5
 
 #Verify
-python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 419980000
+python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 629960000
 echo
 
 tail -n 2 owned_commitments > commitment_file
 commitment=$(awk 'FNR>=1 && FNR<=1' commitment_file)
 echo "\n\n Owned Abars after Bar to Abar conversion 1"
 sleep 20
-target/release/fn owned-abars --commitment $commitment
-fee_commitment=$(awk 'FNR>=2 && FNR<=2' commitment_file)
-echo "\n\n Owned Abars after Bar to Abar conversion 2"
-sleep 20
-target/release/fn owned-abars --commitment $fee_commitment
+target/release/fn owned-abars -c $commitment
+#fee_commitment=$(awk 'FNR>=2 && FNR<=2' commitment_file)
+#echo "\n\n Owned Abars after Bar to Abar conversion 2"
+#sleep 20
+#target/release/fn owned-abars -c $fee_commitment
 
-target/release/fn convert-abar-to-bar --anon-keys ./anon-keys-temp.keys --commitment $commitment --to-wallet-address  fra1ck6mu4fgmh7n3g0y5jm0zjrq6hwgckut9q2tf5fpwhrdgkhgdp9qhla5t5
+target/release/fn convert-abar-to-bar --anon-keys ./anon-keys-temp.keys -c $commitment --to-wallet-address  fra1ck6mu4fgmh7n3g0y5jm0zjrq6hwgckut9q2tf5fpwhrdgkhgdp9qhla5t5
 sleep 20
 #Verify
-python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 629980000
+python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 629960000
 echo
