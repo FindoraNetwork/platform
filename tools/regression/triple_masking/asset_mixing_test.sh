1d4af63bc9cded7d03fc57a3a46a1f0b70cb119b
Sun <sun@findora.org>
2022-05-31 19:56:02 +0800

    fix env in asset mixing test

--- tools/regression/triple_masking/asset_mixing_test.sh
+++ tools/regression/triple_masking/asset_mixing_test.sh
@@ -11,8 +11,6 @@ TM_SLEEP=20
 #Run Tests
 echo -e "${YEL}Run asset mixing test cases and verify results${NC}"
 
-
-
 FRA_ACCOUNT="fra1peaqwykz6vgpvhxfu60w6lngqw6jvwr0f8cyzzvmaqa3dn6xsy4qan9rne"
 BAR_SEC_KEY="KtbqhEo_FJaQoq2fV5K4niayxz_VK8LHJBqU1eV5x0o="
 
@@ -26,16 +24,14 @@ ANON_PK_2="ptyo7hlqn-Eywf_ttG99OSWtz6KfDUvLYDDVDlV1C-U="
 ANON_ENC_2="SAmB7Oji4sAgENLaLb4PFclxQL_DRrEkXcYp6eXuXwI="
 ANON_DEC_2="AEq1ZUFk_fB__YaNjQ3D2taGOnMZAx4adpB6RbnPj24="
 
-ANON_SK_3="GlEc6A-SDpkLUY8meGBW4uTPJig_Doq9Nr0qS6SjCQRg0goM6ovtdrfS2Ofch2RQ3sT7fetbQP3NHf65mm5Tsw=="
-ANON_PK_3="YNIKDOqL7Xa30tjn3IdkUN7E-33rW0D9zR3-uZpuU7M="
-ANON_ENC_3="c_FygTv2K-Vdx6VS-b9retJCgnci19w2C1_ZpRCE2CE="
-ANON_DEC_3="EHr6smaDNCEgCmz2DfkNtnXcDow0QS6Dg9ZXhd_EKH0="
+ANON_SK_3="vw41h9OciN5cDh8QSPFEodSA5AnuHxxcV5SKJ_q-JQMGUX_qogo-lXUvhnXo1gYuP8YsoJJro7kNloXx8wfDSA=="
+ANON_PK_3="BlF_6qIKPpV1L4Z16NYGLj_GLKCSa6O5DZaF8fMHw0g="
+ANON_ENC_3="NBY5yIhdriJVq-7BS59J2IxBgLhewr8TEE6suNc1elA="
+ANON_DEC_3="OKh4F5o_Mw0eKi0xU8lQhi_lXMVd-hem7N12lvSy_WU="
 
-FILE_MNEMONIC="mnemonic-temp.keys"
-FILE_ANON_KEYS_1="anon-keys-temp_1.keys"
+FILE_ANON_KEYS="anon-keys-temp_1.keys"
 FILE_ANON_KEYS_2="anon-keys-temp_2.keys"
 FILE_ANON_KEYS_3="anon-keys-temp_3.keys"
-
 
 set -e
 ./$TRIPLE_MASKING_SCRIPTS_PATH/create_test_bars.sh $FRA_ACCOUNT
@@ -43,34 +39,6 @@ set -e
 python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 840000000
 echo
 set +e
-
-
-# FRA: fra1peaqwykz6vgpvhxfu60w6lngqw6jvwr0f8cyzzvmaqa3dn6xsy4qan9rne
-echo "proof zero oven patrol correct toss silent repair suffer dose equip patch enrich unfold track sea network reduce input aware wild feed inject key" > $FILE_MNEMONIC
-
-echo "
-{
-  \"axfr_secret_key\": \"J7PqRhmBOE_gadFs4rB4lcKuz_YoWa5VSlALyKuZdQjNBryPSYZhRczonGNY3-mp86LWW8TJ6clirfk4gk03Tw==\",
-  \"axfr_public_key\": \"zQa8j0mGYUXM6JxjWN_pqfOi1lvEyenJYq35OIJNN08=\",
-  \"enc_key\": \"Gu558brzFchoqQR9oi8QP54KZKSQ18Djzt82C4YUyFg=\",
-  \"dec_key\": \"4GNC0J_qOXV2kww5BC5bOCyrTEfCodX5BoFaj06uN1s=\"
-}" > $FILE_ANON_KEYS_1
-
-echo "
-{
-  \"axfr_secret_key\": \"MwdsbYhTp4Io062nV7E2HkJfsnaTCZpkdjr6aijv2Aem3KjuGWqf4TLB_-20b305Ja3Pop8NS8tgMNUOVXUL5Q==\",
-  \"axfr_public_key\": \"ptyo7hlqn-Eywf_ttG99OSWtz6KfDUvLYDDVDlV1C-U=\",
-  \"enc_key\": \"SAmB7Oji4sAgENLaLb4PFclxQL_DRrEkXcYp6eXuXwI=\",
-  \"dec_key\": \"AEq1ZUFk_fB__YaNjQ3D2taGOnMZAx4adpB6RbnPj24=\"
-}" > $FILE_ANON_KEYS_2
-
-echo "
-{
-  \"axfr_secret_key\": \"GlEc6A-SDpkLUY8meGBW4uTPJig_Doq9Nr0qS6SjCQRg0goM6ovtdrfS2Ofch2RQ3sT7fetbQP3NHf65mm5Tsw==\",
-  \"axfr_public_key\": \"YNIKDOqL7Xa30tjn3IdkUN7E-33rW0D9zR3-uZpuU7M=\",
-  \"enc_key\": \"c_FygTv2K-Vdx6VS-b9retJCgnci19w2C1_ZpRCE2CE=\",
-  \"dec_key\": \"EHr6smaDNCEgCmz2DfkNtnXcDow0QS6Dg9ZXhd_EKH0=\"
-}" > $FILE_ANON_KEYS_3
 
 rm owned_commitments
 rm sent_commitments
@@ -85,14 +53,14 @@ $BIN/fn setup -O "$TM_REGRESSION_PATH"/mnemonic.key > /dev/null
 echo "\n\n FRA Bar To Abar ..."
 echo "==============================================================================="
 TXO_SID=$(target/release/fn owned-utxos | head -4 | tail -1 |  awk -F ' ' '{print $1}')
-target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS_1 --txo-sid "$TXO_SID"
+target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS --txo-sid "$TXO_SID"
 echo "waiting blockchain 20s..."
 sleep 30
 
 #Verify FRA balance
 python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 629980000
 commitment1=$(tail -n 1 owned_commitments)
-python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS_1 --commitments $commitment1 --amount 210000000
+python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS --commitments $commitment1 --amount 210000000
 echo
 
 echo "\n\n Create Asset 1 ..."
@@ -140,25 +108,25 @@ target/release/fn owned-utxos
 echo "\n\n Asset 1 Bar To Abar ..."
 echo "==============================================================================="
 TXO_SID=$(target/release/fn owned-utxos --asset "$ASSET1" | head -4 | tail -1 | awk -F ' ' '{print $1}')
-target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS_1 --txo-sid "$TXO_SID"
+target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS --txo-sid "$TXO_SID"
 echo "waiting blockchain 20s..."
 sleep $TM_SLEEP
 
 echo "\n\n Asset 2 Bar To Abar ..."
 echo "==============================================================================="
 TXO_SID=$(target/release/fn owned-utxos --asset "$ASSET2" | head -4 | tail -1 | awk -F ' ' '{print $1}')
-target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS_1 --txo-sid "$TXO_SID"
+target/release/fn convert-bar-to-abar --anon-keys $FILE_ANON_KEYS --txo-sid "$TXO_SID"
 echo "waiting blockchain 20s..."
 sleep $TM_SLEEP
 
 #Verify balance for custom assets
 python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 0 --asset $ASSET1
 commitmentAsset1=$(tail -n 2 owned_commitments | head -n 1)
-python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS_1 --commitments $commitmentAsset1 --amount 100000000 --asset $ASSET1
+python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS --commitments $commitmentAsset1 --amount 100000000 --asset $ASSET1
 echo
 python $REGRESSION_PATH/evm.py verify-balance --sec-key $BAR_SEC_KEY --amount 0 --asset $ASSET2
 commitmentAsset2=$(tail -n 1 owned_commitments)
-python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS_1 --commitments $commitmentAsset2 --amount 100000000 --asset $ASSET2
+python $REGRESSION_PATH/evm.py verify-anon-balance --anon-keys ./$FILE_ANON_KEYS --commitments $commitmentAsset2 --amount 100000000 --asset $ASSET2
 echo
 
 #Verify FRA balance
@@ -171,7 +139,7 @@ COMMITMENT=$(awk 'FNR==3' owned_commitments)
 FRA_COMMITMENT=$(awk 'FNR==2' owned_commitments)
 target/release/fn anon-transfer    \
   --amount 50000000                \
-  --anon-keys $FILE_ANON_KEYS_1    \
+  --anon-keys $FILE_ANON_KEYS    \
   --commitment $COMMITMENT         \
   --fra-commitment $FRA_COMMITMENT \
   --to-axfr-public-key $ANON_PK_2  \
