0fab5f0ae1effd4b3fb6a1786de4fc4fe7adb72a
Charlie Chen <charlie@findora.org>
2022-07-11 22:54:15 -0500

    my test branch

--- tools/devnet/resetnodes.sh
+++ tools/devnet/resetnodes.sh
@@ -4,12 +4,16 @@
 source tools/devnet/env.sh || exit 1
 
 # get params if provided
-V="3"
+V="1"
 N="1"
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
     V=$1
     N=$2
 fi
+
+# create folder if necessary
+mkdir -p $HOME/.findora
+mkdir -p $DEVNET
 
 # clean nodes
 rm -rf $DEVNET/*
