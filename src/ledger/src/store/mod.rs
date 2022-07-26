2d68151d83a4bc14865ac9babd7a9995eb72d6e2
Harshad Patil <harshad0402@gmail.com>
2022-07-26 16:14:48 -0700

    fix lint error

--- src/ledger/src/store/mod.rs
+++ src/ledger/src/store/mod.rs
@@ -1097,12 +1097,10 @@ impl LedgerState {
     pub fn get_state_commitment(&self) -> (HashOf<Option<StateCommitmentData>>, u64) {
         let block_count = self.status.block_commit_count;
         if !*KEEP_HIST {
-            let commitment = self
-                .status
-                .state_commitment_versions
-                .iter()
-                .last()
-                .unwrap_or_else(|| HashOf::new(&None));
+            let mut commitment:HashOf<Option<StateCommitmentData>> = HashOf::new(&None);
+            for a in self.status.state_commitment_versions.iter() {
+                commitment = a.clone();
+            }
             (commitment, block_count)
         } else {
             let commitment = self
