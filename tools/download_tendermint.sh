ac2c7d6a82f721cb3fb769f57eb6f0a53e3bafc2
cisco <codingmaster.wang@outlook.com>
2022-08-12 15:26:06 +0800

    download tendermint-release

--- tools/download_tendermint.sh
+++ tools/download_tendermint.sh
@@ -3,18 +3,32 @@
 platform=$1
 
 if [ ! -f "./tools/tendermint.zip" ]; then
-    if [ "MacOS" == "${platform}" ]; then
-      wget -O ./tools/tendermint.zip "https://github.com/tendermint/tendermint/releases/download/v0.33.9/tendermint_v0.33.9_darwin_amd64.zip"
-    elif [ "Linux" == "${platform}" ]; then
-      wget -O ./tools/tendermint.zip "https://github.com/tendermint/tendermint/releases/download/v0.33.9/tendermint_v0.33.9_linux_amd64.zip"
-    fi
+  if [ "MacOS" == "${platform}" ]; then
+    wget -O ./tools/tendermint.zip "https://github.com/tendermint/tendermint/releases/download/v0.33.9/tendermint_v0.33.9_darwin_amd64.zip"
+  elif [ "Linux" == "${platform}" ]; then
+    wget -O ./tools/tendermint.zip "https://github.com/tendermint/tendermint/releases/download/v0.33.9/tendermint_v0.33.9_linux_amd64.zip"
+  fi
 else
-    echo "tendermint.zip already exists."
+  echo "tendermint.zip already exists."
 fi
 
-if ! [ -x "$(unzip -d ./tools/ ./tools/tendermint.zip)" ]; then
-    echo 'please install unzip'
-    exit
+if ! [ -x "$(command -v unzip)" ]; then
+  echo 'error:'
+  echo 'please install unzip.' >&2
+  exit 1
+else
+  unzip -d ./tools/ ./tools/tendermint.zip
 fi
+
+if [ ! -f "./tools/tendermint" ]; then
+  rm ./tools/tendermint.zip
+  rm ./tools/tendermint
+  echo 'error:'
+  echo 'unzip ./tools/tendermint.zip failed.'
+  echo 'Make sure ./tools/tendermint.zip is a complete file, or try again.' >&2
+  exit 1
+fi
+
+
 
 
