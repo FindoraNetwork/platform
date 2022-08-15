ff7a98778715cd92d816276444da2f2d0d08b239
ccmlm <fanhui.x@gmail.com>
2022-08-15 21:31:05 +0800

    Revert "update btm crate version (#440)"
    This reverts commit ec46b792954083c678aac674a8c0368a3875c9f2.

--- src/components/config/src/abci/mod.rs
+++ src/components/config/src/abci/mod.rs
@@ -441,18 +441,17 @@ pub mod global_cfg {
             if let Some(sm) = m.value_of("snapshot-mode") {
                 res.mode = SnapMode::from_string(sm).map_err(|e| d!("{}", e))?;
                 if !matches!(res.mode, SnapMode::External) {
-                    res.volume = m
+                    res.target = m
                         .value_of("snapshot-target")
                         .c(d!("Missing `snapshot-target`."))?
                         .to_owned();
                 }
             } else {
-                res.volume = m
+                res.target = m
                     .value_of("snapshot-target")
                     .c(d!("Missing `snapshot-target`."))?
                     .to_owned();
-                res.mode =
-                    BtmCfg::guess_mode(res.volume.as_str()).map_err(|e| d!("{}", e))?;
+                res.mode = res.guess_mode().map_err(|e| d!("{}", e))?;
             }
 
             if let Some(sa) = m.value_of("snapshot-algo") {
@@ -467,14 +466,13 @@ pub mod global_cfg {
             || m.is_present("snapshot-rollback-to-exact")
         {
             // this field should be parsed at the top
-            res.volume = m
+            res.target = m
                 .value_of("snapshot-target")
                 .c(d!("Missing `snapshot-target`."))?
                 .to_owned();
 
             // the guess should always success in this scene
-            res.mode =
-                BtmCfg::guess_mode(res.volume.as_str()).map_err(|e| d!("{}", e))?;
+            res.mode = res.guess_mode().map_err(|e| d!("{}", e))?;
 
             if m.is_present("snapshot-list") {
                 list_snapshots(&res).map_err(|e| d!("{}", e))?;
