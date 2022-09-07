0b9621920c3b2dcaeaa3e85c34c0b7252b2b32f2
harry <harry@findora.org>
2022-09-07 20:05:04 +0800

    ci test

--- src/components/config/src/abci/mod.rs
+++ src/components/config/src/abci/mod.rs
@@ -36,6 +36,7 @@ pub struct CheckPointConfig {
     pub utxo_asset_prefix_height: u64,
     pub prism_bridge_address: String,
     pub nonce_bug_fix_height: u64,
+    pub ci_test_value: u64,
 }
 
 impl CheckPointConfig {
@@ -69,6 +70,7 @@ impl CheckPointConfig {
                                 utxo_asset_prefix_height: 0,
                                 nonce_bug_fix_height: 0,
                                 prism_bridge_address: String::new(),
+                                ci_test_value: 0,
                             };
                             #[cfg(not(feature = "debug_env"))]
                             let config = CheckPointConfig {
@@ -92,6 +94,7 @@ impl CheckPointConfig {
                                 utxo_asset_prefix_height: 30000000,
                                 nonce_bug_fix_height: 30000000,
                                 prism_bridge_address: String::new(),
+                                ci_test_value: 30000000,
                             };
                             let content = toml::to_string(&config).unwrap();
                             file.write_all(content.as_bytes()).unwrap();
