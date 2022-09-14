130c36d12a677fcf787366acdaf9ca074a9c17de
simonjiao <simon@findora.org>
2022-09-15 00:26:21 +0800

    honor block_number in call rpc

--- src/components/contracts/rpc/src/eth.rs
+++ src/components/contracts/rpc/src/eth.rs
@@ -317,7 +317,7 @@ impl EthApi for EthApiImpl {
     fn call(
         &self,
         request: CallRequest,
-        _: Option<BlockNumber>,
+        block_number: Option<BlockNumber>,
     ) -> BoxFuture<Result<Bytes>> {
         debug!(target: "eth_rpc", "call, request:{:?}", request);
 
@@ -334,18 +334,14 @@ impl EthApi for EthApiImpl {
                 nonce,
             } = request;
 
-            let block = account_base_app.read().current_block(None);
+            let id = native_block_id(block_number);
+            let block = account_base_app
+                .read()
+                .current_block(id)
+                .ok_or_else(|| internal_err("failed to get block"))?;
+
             // use given gas limit or query current block's limit
-            let gas_limit = match gas {
-                Some(amount) => amount,
-                None => {
-                    if let Some(block) = block.clone() {
-                        block.header.gas_limit
-                    } else {
-                        <BaseApp as module_evm::Config>::BlockGasLimit::get()
-                    }
-                }
-            };
+            let gas_limit = gas.unwrap_or(block.header.gas_limit);
             let data = data.map(|d| d.0).unwrap_or_default();
 
             let mut config = <BaseApp as module_ethereum::Config>::config().clone();
@@ -353,18 +349,14 @@ impl EthApi for EthApiImpl {
 
             let mut ctx = account_base_app
                 .read()
-                .create_query_context(None, false)
-                .map_err(|err| {
-                    internal_err(format!("create query context error: {:?}", err))
-                })?;
-            if let Some(block) = block {
-                ctx.header
-                    .mut_time()
-                    .set_seconds(block.header.timestamp as i64);
-                ctx.header.height = block.header.number.as_u64() as i64;
-                ctx.header.proposer_address =
-                    Vec::from(block.header.beneficiary.as_bytes())
-            }
+                .create_context_at(block.header.number.as_u64())
+                .ok_or_else(|| internal_err("failed to create context"))?;
+
+            ctx.header
+                .mut_time()
+                .set_seconds(block.header.timestamp as i64);
+            ctx.header.height = block.header.number.as_u64() as i64;
+            ctx.header.proposer_address = Vec::from(block.header.beneficiary.as_bytes());
 
             match to {
                 Some(to) => {
