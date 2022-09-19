facf600044ed45763f1b1d845643c657cb891849
18328594608 <1007697416@qq.com>
2022-09-19 10:14:12 +0800

    Revert "FO-1251 <Rollback the merge ：FO-846>"
    This reverts commit 81d49021158744637f02105a9e72c85c00001c8d.

--- src/components/contracts/baseapp/src/app.rs
+++ src/components/contracts/baseapp/src/app.rs
@@ -198,13 +198,7 @@ impl crate::BaseApp {
 
         let block_height = self.deliver_state.block_header().height as u64;
         //let mut ctx = self.retrieve_context(RunTxMode::Deliver).clone(); LOCAL FIXED
-        self.deliver_state
-            .db
-            .write()
-            .commit(block_height)
-            .unwrap_or_else(|_| {
-                panic!("Failed to commit chain db at height: {}", block_height)
-            });
+
         // Write the DeliverTx state into branched storage and commit the Store.
         // The write to the DeliverTx state writes all state transitions to the root
         // Store so when commit() is called is persists those values.
