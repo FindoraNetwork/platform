32650dae486df55b1af5f8dc7e37205bb798bb54
onewayfunc <onewayfunc@gmail.com>
2022-11-14 11:39:04 -0800

    change the file name to fit the image

--- src/ledger/src/data_model/debug_logger.rs
+++ src/ledger/src/data_model/debug_logger.rs
@@ -6,7 +6,7 @@ pub fn add_log(str: String) {
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("/home/ubuntu/debug_log")
+        .open("/root/debug_log")
         .unwrap();
 
     if let Err(e) = writeln!(file, "{}", str) {
