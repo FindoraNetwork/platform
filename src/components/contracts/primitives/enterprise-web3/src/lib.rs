9ee19d23b2ed96e168a756e4bc0212856520a88a
shaorongqiang <shaorongqiang@aliyun.com>
2022-12-04 17:29:29 +0800

    add r2d2 redis pool

--- src/components/contracts/primitives/enterprise-web3/src/lib.rs
+++ src/components/contracts/primitives/enterprise-web3/src/lib.rs
@@ -1,13 +1,15 @@
 use evm_exporter::{
-    Block as EnterpriseBlock, Getter, Receipt as EnterpriseReceipt, Setter,
+    Block as EnterpriseBlock, Getter, Receipt as EnterpriseReceipt,
     State as EnterpriseState, TransactionStatus as EnterpriseTxState,
 };
 use lazy_static::lazy_static;
 use primitive_types::{H160, H256, U256};
-use redis::{Client, Connection};
+use redis::Client;
 use ruc::*;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+
+pub use evm_exporter::Setter;
 
 pub type State = EnterpriseState;
 pub type Block = EnterpriseBlock;
@@ -26,7 +28,7 @@ lazy_static! {
     pub static ref BLOCK: Arc<Mutex<Option<Block>>> = Arc::new(Mutex::new(None));
     pub static ref RECEIPTS: Arc<Mutex<Vec<Receipt>>> = Arc::new(Mutex::new(vec![]));
     pub static ref TXS: Arc<Mutex<Vec<TxState>>> = Arc::new(Mutex::new(vec![]));
-    static ref REDIS_CLIENT: Arc<Mutex<Client>> =
+    pub static ref REDIS_CLIENT: Arc<Mutex<r2d2::Pool<Client>>> =
         Arc::new(Mutex::new(gen_redis_client()));
     pub static ref WEB3_SERVICE_START_HEIGHT: u64 = load_start_height();
     pub static ref PENDING_CODE_MAP: Arc<Mutex<HashMap<H160, Vec<u8>>>> =
@@ -39,21 +41,17 @@ lazy_static! {
         Arc::new(Mutex::new(vec![]));
 }
 
-fn gen_redis_client() -> Client {
+fn gen_redis_client() -> r2d2::Pool<Client> {
     let redis_addr = std::env::var("REDIS_ADDR").unwrap();
-    let client = Client::open(redis_addr).c(d!()).unwrap();
-    client
+    let client = Client::open(redis_addr).unwrap();
+    let pool = pnk!(r2d2::Pool::builder().max_size(50).build(client));
+    pool
 }
 
 fn load_start_height() -> u64 {
-    let client = REDIS_CLIENT.lock().c(d!()).unwrap();
-    let mut conn = client.get_connection().c(d!()).unwrap();
-    let mut getter = Getter::new(&mut conn, "evm".to_string());
-    getter.latest_height().unwrap() as u64
-}
-
-pub fn setter() -> Result<Setter<Connection>> {
-    let client = REDIS_CLIENT.lock().c(d!())?;
-    let con = client.get_connection().c(d!())?;
-    Ok(Setter::new(con, "evm".to_string()))
+    let redis_pool = REDIS_CLIENT.lock().unwrap();
+    let mut conn = redis_pool.get().expect("get redis connect");
+    let mut getter = Getter::new(&mut *conn, "evm".to_string());
+    let last_height = getter.latest_height().expect("redis latest_height error");
+    last_height as u64
 }
