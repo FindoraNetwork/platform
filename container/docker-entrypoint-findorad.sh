20e3953e4d958df1842b5c9c692f1cfc2ed0f3f0
Jimmy <jimmy@findora.org>
2023-01-11 19:06:04 -0800

    include more variable in start command
    Update docker-entrypoint-findorad.sh
    
    Update docker-entrypoint-findorad.sh

--- container/docker-entrypoint-findorad.sh
+++ container/docker-entrypoint-findorad.sh
@@ -3,14 +3,29 @@
 export PORT=8080
 export RPCURL=http://127.0.0.1:8545
 export NETWORK=PRINET
+export MODE=ONLINE
 
-if [ ! -n "$MODE" ]; then
-    export MODE=ONLINE
-fi
 
 if [ ! -z $ROSETTA ]; then 
     if [ $ROSETTA == true ]; then
+        if [ ! -z $ROSETTA_PORT ]; then 
+            export PORT=$ROSETTA_PORT
+        fi
+
+        if [ ! -z $ROSETTA_RPCURL ]; then 
+            export RPCURL=$ROSETTA_RPCURL
+        fi
+
+        if [ ! -z $ROSETTA_NETWORK ]; then 
+            export NETWORK=$ROSETTA_NETWORK
+        fi
+        
+        if [ ! -z $ROSETTA_MODE ]; then 
+            export MODE=$ROSETTA_MODE
+        fi
         nohup findora-rosetta run > /dev/null &
+
+
     fi
 fi
 
