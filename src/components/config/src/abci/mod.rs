c2b27c5317b1ab91b67faaedb33113dbcd62d596
simonjiao <simonjiaoh@gmail.com>
2023-02-01 11:13:32 +0800

    Fix reentrant hang issue (#707)
    * fix unhandled transaction
    
    * fix evm deliver_tx nonce
    
    * update startnodes.sh and checkpoint file
    
    ---------
    
    Co-authored-by: simonjiao <simon@findora.org>

--- src/components/config/src/abci/mod.rs
+++ src/components/config/src/abci/mod.rs
@@ -88,6 +88,10 @@ pub struct CheckPointConfig {
     pub fix_delegators_am_height: u64,
     pub validators_limit_v2_height: u64,
     pub enable_eip1559_height: u64,
+
+    // https://github.com/FindoraNetwork/platform/pull/707
+    // FO-1370: V0.3.30 EVM bug: receipt missing when error code === 1
+    pub fix_deliver_tx_revert_nonce_height: i64,
 }
 
 impl CheckPointConfig {
@@ -129,6 +133,7 @@ impl CheckPointConfig {
                                 fix_delegators_am_height: 0,
                                 validators_limit_v2_height: 0,
                                 enable_eip1559_height: 0,
+                                fix_deliver_tx_revert_nonce_height: 0,
                             };
                             #[cfg(not(feature = "debug_env"))]
                             let config = CheckPointConfig {
@@ -158,6 +163,7 @@ impl CheckPointConfig {
                                 fix_delegators_am_height: 30000000,
                                 validators_limit_v2_height: 30000000,
                                 enable_eip1559_height: 40000000,
+                                fix_deliver_tx_revert_nonce_height: 40000000,
                             };
                             let content = toml::to_string(&config).unwrap();
                             file.write_all(content.as_bytes()).unwrap();
