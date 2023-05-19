8db1dde1b1cb87b22868504c389e3f9f310f5c07
shaorongqiang <shaorongqiang@gmail.com>
2023-05-19 15:33:10 +0800

    feat imprison loser

--- tools/download_tendermint.sh
+++ tools/download_tendermint.sh
@@ -5,5 +5,5 @@ target_path=$1
 if [[ ! -d "${target_path}/.git" ]]; then
     rm -rf $target_path
     url='https://github.com/FindoraNetwork/tendermint.git'
-    git clone -b findora-v0.33.9p3 --depth=1 $url $target_path
+    git clone -b feat-imprison-loser --depth=1 $url $target_path
 fi
