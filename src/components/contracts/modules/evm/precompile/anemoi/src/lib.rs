6a7cf842ce2f54f53dd01f850578e4f58715923e
Sun <sun@findora.org>
2023-05-30 17:04:14 +1200

    Update precompiled anemni

--- src/components/contracts/modules/evm/precompile/anemoi/src/lib.rs
+++ src/components/contracts/modules/evm/precompile/anemoi/src/lib.rs
@@ -1,9 +1,13 @@
+use ethereum_types::U256;
 use evm::executor::stack::{PrecompileFailure, PrecompileOutput};
 use evm::{Context, ExitError, ExitSucceed};
+use evm_precompile_utils::EvmDataReader;
 use module_evm::precompile::{FinState, Precompile, PrecompileId, PrecompileResult};
 use platform_lib_noah::noah_algebra::bls12_381::BLSScalar;
 use platform_lib_noah::noah_algebra::prelude::Scalar;
-use platform_lib_noah::noah_crypto::basic::anemoi_jive::{AnemoiJive, AnemoiJive381};
+use platform_lib_noah::noah_crypto::basic::anemoi_jive::{
+    AnemoiJive, AnemoiJive381, ANEMOI_JIVE_381_SALTS,
+};
 
 /// The Anemoi precompile
 pub struct Anemoi;
@@ -11,7 +15,55 @@ pub struct Anemoi;
 impl Anemoi {
     pub const GAS_PER_PERM: u64 = 125000;
 
-    pub fn execute_with_input_and_gas(
+    pub fn eval_jive(input: &[u8], target_gas: Option<u64>) -> PrecompileResult {
+        let gas_cost = 2 * Self::GAS_PER_PERM;
+        if let Some(gas_left) = target_gas {
+            if gas_left < gas_cost {
+                return Err(PrecompileFailure::Error {
+                    exit_status: ExitError::OutOfGas,
+                });
+            }
+        };
+
+        // 32 * 4 = 128
+        if input.len() != 128 {
+            return Err(PrecompileFailure::Error {
+                exit_status: ExitError::Other("input must be 128 bytes".into()),
+            });
+        }
+
+        let mut fields = Vec::with_capacity(3);
+
+        for bytes in input[..96].chunks(32) {
+            let le_bytes: Vec<u8> = bytes.iter().rev().copied().collect();
+            if let Ok(res) = BLSScalar::from_bytes(&le_bytes) {
+                fields.push(res);
+            } else {
+                return Err(PrecompileFailure::Error {
+                    exit_status: ExitError::Other(
+                        "Cannot convert bytes to field elements".into(),
+                    ),
+                });
+            }
+        }
+        let index = U256::from_big_endian(&input[96..]);
+
+        let mut res = AnemoiJive381::eval_jive(
+            &[fields[0], fields[1]],
+            &[fields[2], ANEMOI_JIVE_381_SALTS[index.as_usize()]],
+        )
+        .to_bytes();
+        res.reverse();
+
+        Ok(PrecompileOutput {
+            exit_status: ExitSucceed::Returned,
+            cost: gas_cost,
+            output: res,
+            logs: Default::default(),
+        })
+    }
+
+    pub fn eval_variable_length_hash(
         input: &[u8],
         target_gas: Option<u64>,
     ) -> PrecompileResult {
@@ -34,18 +86,12 @@ impl Anemoi {
             }
         };
 
-        let mut field_elems = Vec::with_capacity(num_elems);
+        let mut fields = Vec::with_capacity(num_elems);
 
-        for i in 0..num_elems {
-            let res = BLSScalar::from_bytes(
-                &input[i * 32..(i + 1) * 32]
-                    .iter()
-                    .rev()
-                    .copied()
-                    .collect::<Vec<u8>>(),
-            );
-            if let Ok(res) = res {
-                field_elems.push(res);
+        for bytes in input.chunks(32) {
+            let le_bytes: Vec<u8> = bytes.iter().rev().copied().collect();
+            if let Ok(res) = BLSScalar::from_bytes(&le_bytes) {
+                fields.push(res);
             } else {
                 return Err(PrecompileFailure::Error {
                     exit_status: ExitError::Other(
@@ -55,16 +101,23 @@ impl Anemoi {
             }
         }
 
-        let mut res = AnemoiJive381::eval_variable_length_hash(&field_elems).to_bytes();
+        let mut res = AnemoiJive381::eval_variable_length_hash(&fields).to_bytes();
         res.reverse();
 
         Ok(PrecompileOutput {
             exit_status: ExitSucceed::Returned,
             cost: gas_cost,
-            output: res.to_vec(),
+            output: res,
             logs: Default::default(),
         })
     }
+}
+
+#[evm_precompile_utils::generate_function_selector]
+#[derive(Debug, PartialEq, Eq, num_enum::TryFromPrimitive, num_enum::IntoPrimitive)]
+pub enum Call {
+    EvalJive = "eval_jive(bytes32,bytes32,bytes32,uint256)",
+    EvalVariableLengthHash = "eval_variable_length_hash(bytes32[])",
 }
 
 impl PrecompileId for Anemoi {
@@ -80,6 +133,19 @@ impl Precompile for Anemoi {
         _context: &Context,
         _state: &FinState,
     ) -> PrecompileResult {
-        Self::execute_with_input_and_gas(input, target_gas)
+        let mut input = EvmDataReader::new(input);
+        let selector = match input.read_selector::<Call>() {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(PrecompileFailure::Error { exit_status: e });
+            }
+        };
+
+        match &selector {
+            Call::EvalJive => Self::eval_jive(input.get_slice(), target_gas),
+            Call::EvalVariableLengthHash => {
+                Self::eval_variable_length_hash(input.get_slice(), target_gas)
+            }
+        }
     }
 }
