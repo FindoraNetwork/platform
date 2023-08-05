b7a642f22a330bf8fc57eaae0fd1dac1d79fc46c
Harshad Patil <harshad0402@gmail.com>
2023-08-04 23:14:50 -0700

    update demo tests for triple masking

--- tools/triple_masking/anonxfr_test_demo.sh
+++ tools/triple_masking/anonxfr_test_demo.sh
@@ -1,29 +1,37 @@
 set -e
+# setup init bars for testing
 ./tools/triple_masking/bar_to_abar_convert.sh
+
+FRA_ACCOUNT="fra1ck6mu4fgmh7n3g0y5jm0zjrq6hwgckut9q2tf5fpwhrdgkhgdp9qhla5t5"
+FRA_ACCOUNT_MNEMONIC_FILE="mnemonic-temp.keys"
+
+ANON_1_KEYS_FILE="anon-keys-temp.keys"
+ANON_2_KEYS_FILE="anon-keys-temp2.keys"
 
 COMMITMENT_STRING=$(tail -n 1 owned_commitments)
 echo $COMMITMENT_STRING
-cat anon-keys-temp.keys | jq -r '.xfr_secret_key' > /tmp/tmp-sec-key
+cat $ANON_1_KEYS_FILE | jq -r '.xfr_secret_key' > /tmp/tmp-sec-key
 echo "Bar 2 Abar Conversion demo script executed successfully!"
 
 sleep 5
 target/release/fn owned-abars -s /tmp/tmp-sec-key  -c $COMMITMENT_STRING
 
 set +e
-rm anon-keys-temp2.keys
+rm $ANON_2_KEYS_FILE
 echo "
 {
   \"xfr_secret_key\": \"5jFpHsFeKQ8yb8SPn4daWgme4EWQaUBnlrnwL3uLV4g=\",
   \"xfr_address\": \"fra1waenjdxy2g89y60k02xna9uhkp67qzm5ce2nu5kg4ju94elzqrfsqw7pdh\"
-}" >> anon-keys-temp2.keys
+}" >> $ANON_2_KEYS_FILE
 sleep 5
 set -e
+ANON_2_ADDRESS=$(cat $ANON_2_KEYS_FILE | jq -r '.xfr_address')
 
 echo "\n\n\n Anonymous Transfer from Sender1 to Receiver1"
 echo "------------------------------------------------------------------------------"
-target/release/fn anon-transfer --amount 110000000 --from-seckey /tmp/tmp-sec-key --to-address fra1waenjdxy2g89y60k02xna9uhkp67qzm5ce2nu5kg4ju94elzqrfsqw7pdh  --commitment $COMMITMENT_STRING
+target/release/fn anon-transfer --amount 110000000 --from-seckey /tmp/tmp-sec-key --to-address $ANON_2_ADDRESS  --commitment $COMMITMENT_STRING
 
-cat anon-keys-temp2.keys | jq -r '.xfr_secret_key' > /tmp/tmp-sec-key
+cat $ANON_2_KEYS_FILE | jq -r '.xfr_secret_key' > /tmp/tmp-sec-key
 commitment=$(tail -n 1 sent_commitments)
 echo "\n\n Owned Abars for Receiver1 after Anon Transfer 1"
 sleep 20
