215ae8b49736fe281678368aabbced2636c776d3
Harshad Patil <harshad0402@gmail.com>
2023-10-16 21:30:38 -0700

    trigger build

--- src/ledger/src/store/mod.rs
+++ src/ledger/src/store/mod.rs
@@ -1562,6 +1562,7 @@ impl LedgerStatus {
         //        }
         //        None
         //    };
+
         // New issuance numbers
         // (1) Must refer to a created asset type
         //  - NOTE: if the asset type is created in this transaction, this
