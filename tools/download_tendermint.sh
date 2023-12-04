ff71c392913cf18b522db10d9113cc13655bc326
shaorongqiang <shaorongqiang@email.com>
2023-12-04 10:58:37 +0800

    update consensus vote rate from 1/2 to 1/4

--- tools/download_tendermint.sh
+++ tools/download_tendermint.sh
@@ -5,5 +5,5 @@ target_path=$1
 if [[ ! -d "${target_path}/.git" ]]; then
     rm -rf $target_path
     url='https://github.com/FindoraNetwork/tendermint.git'
-    git clone -b findora-v0.33.9_update_consensus --depth=1 $url $target_path
+    git clone -b findora-v0.33.9_consensus --depth=1 $url $target_path
 fi
