391583b0283ee5d2771bffe0a3d4108c0da15f31
tommady <tommady@users.noreply.github.com>
2024-07-19 03:14:23 +0000

    change primitives/enterprise-web3 from using redis into postgres

--- src/components/contracts/primitives/enterprise-web3/src/lib.rs
+++ src/components/contracts/primitives/enterprise-web3/src/lib.rs
@@ -1,15 +1,12 @@
 use evm_exporter::{
-    Block as EnterpriseBlock, Getter, Receipt as EnterpriseReceipt,
-    State as EnterpriseState, TransactionStatus as EnterpriseTxState,
+    Block as EnterpriseBlock, Getter, PgGetter, PgSetter, Receipt as EnterpriseReceipt,
+    Setter, State as EnterpriseState, TransactionStatus as EnterpriseTxState,
 };
 use lazy_static::lazy_static;
 use primitive_types::{H160, H256, U256};
-use redis::Client;
 use ruc::*;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-
-pub use evm_exporter::Setter;
 
 pub type State = EnterpriseState;
 pub type Block = EnterpriseBlock;
@@ -33,7 +30,7 @@ lazy_static! {
     pub static ref BLOCK: Arc<Mutex<Option<Block>>> = Arc::new(Mutex::new(None));
     pub static ref RECEIPTS: Arc<Mutex<Vec<Receipt>>> = Arc::new(Mutex::new(vec![]));
     pub static ref TXS: Arc<Mutex<Vec<TxState>>> = Arc::new(Mutex::new(vec![]));
-    pub static ref REDIS_CLIENT: Arc<Mutex<r2d2::Pool<Client>>> =
+    pub static ref REDIS_CLIENT: Arc<Mutex<dyn Setter>> =
         Arc::new(Mutex::new(gen_redis_client()));
     pub static ref WEB3_SERVICE_START_HEIGHT: u64 = load_start_height();
     pub static ref PENDING_CODE_MAP: Arc<Mutex<HashMap<H160, Vec<u8>>>> =
@@ -49,17 +46,14 @@ lazy_static! {
         Arc::new(Mutex::new(Vec::new()));
 }
 
-fn gen_redis_client() -> r2d2::Pool<Client> {
-    let redis_addr = std::env::var("REDIS_ADDR").unwrap();
-    let client = Client::open(redis_addr).unwrap();
-    let pool = pnk!(r2d2::Pool::builder().max_size(50).build(client));
-    pool
+fn gen_redis_client() -> dyn Setter {
+    let uri = std::env::var("POSTGRES_URI").expect("loading env POSTGRES_URI failed");
+    PgSetter::new(ConnectionType::Postgres(uri), String::new())
 }
 
 fn load_start_height() -> u64 {
-    let redis_pool = REDIS_CLIENT.lock().unwrap();
-    let mut conn = redis_pool.get().expect("get redis connect");
-    let mut getter = Getter::new(&mut *conn, "evm".to_string());
+    let uri = std::env::var("POSTGRES_URI").expect("loading env POSTGRES_URI failed");
+    let getter: dyn Getter = PgGetter::new(ConnectionType::Postgres(uri), String::new());
     let last_height = getter.latest_height().expect("redis latest_height error");
     last_height as u64
 }
